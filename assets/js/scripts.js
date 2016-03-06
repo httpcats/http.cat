4250733e8cdf3e3d699e8569938d4f4a3fa329c8
Pedro Borges <pedrodavidborges@gmail.com>
2016-03-06 18:48:00 +0000

    httpcat is fucking DONE

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,5 @@
 jQuery(document).ready(function($){
 
-	// Modal
-	Ink.requireModules( ['Ink.Dom.Selector_1','Ink.UI.Modal_1'], function( Selector, Modal ){
-        var modalElement = Ink.s('#desktopModal');
-        var modalObj = new Modal( modalElement );
-    });
 
-    // Tabs
-    Ink.requireModules( ['Ink.Dom.Selector_1','Ink.UI.Tabs_1'], function( Selector, Tabs ){
-        var tabsObj = new Tabs('#protoTabs');
-    });
-
-	// Animations
-	$(".rwd-header").addClass("animated fadeInDown");
-	$(".project-name").addClass("animated fadeInDown delay");
-	$(".project-description").addClass("animated fadeInDown delay");
-	$(".credits").addClass("animated fadeInUp");
-	$(".sprite.brand-logo").addClass("animated fadeIn");
 
 });
