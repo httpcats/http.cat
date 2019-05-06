4680ced5532008bb8e4d1160282e97c8df8b122d
rogeriopvl <rogeriopvl@gmail.com>
2019-05-06 19:35:15 +0100

    feat: add TwitterButton component

---
+++ src/components/TwitterButton/TwitterButton.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import twitterIcon from '../../assets/images/twitter.svg'
+
+const TwitterButton = ({ width, height}) => (
+  <div>
+    <a
+      href="https://twitter.com/rogeriopvl"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src={twitterIcon}
+        alt="Twitter logo"
+        width={width}
+        height={height}
+      />
+    </a>
+  </div>
+)
+
+TwitterButton.defaultProps = {
+  width: '50px',
+  height: '50px'
+}
+
+export default TwitterButton
