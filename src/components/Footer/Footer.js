66ea538cc66735729d3df45af96d854e63bb2a3f
rogeriopvl <rogeriopvl@gmail.com>
2019-05-09 23:12:26 +0100

    feat: add social links to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import FacebookButton from '../FacebookButton'
+import GithubButton from '../GithubButton'
+import TwitterButton from '../TwitterButton'
 import styles from './Footer.module.css'
 
 const Footer = () => (
   <div className={styles.container}>
+    <div className={styles.social}>
+      <FacebookButton />
+      <GithubButton />
+      <TwitterButton />
+    </div>
     <p>Developed by <a href="https://twitter.com/rogeriopvl">@rogeriopvl</a></p>
     <p>Images by Tomomi Imura (<a href="https://twitter.com/girlie_mac">@girlie_mac</a>)</p>
   </div>
