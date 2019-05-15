f9109f701349be83b519f15ea2a08bf3cef751d7
rogeriopvl <rogeriopvl@gmail.com>
2019-05-15 23:26:49 +0100

    feat: add pre-render step after build
    This renders the js bundle into static markup, improving the overall
    performance of the web app.
    
    The tool used for this is react-snap.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { hydrate, render } from 'react-dom'
 
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = document.getElementById('root')
+
+root.hasChildNodes() ? hydrate(<App />, root) : render(<App />, root)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
