44c08a435644a9b0636beff8b45bf1e40746da92
rogeriopvl <rogerio@hey.com>
2020-07-10 18:23:17 +0100

    feat(footer): add abstract api sponsor message

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -20,6 +20,8 @@ const Footer = () => (
 
     <p>{getString('DEVELOPED_BY')} <a href="https://twitter.com/rogeriopvl">@rogeriopvl</a></p>
     <p>{getString('IMAGES_BY')} Tomomi Imura (<a href="https://twitter.com/girlie_mac">@girlie_mac</a>)</p>
+
+      <p>Sponsored by <a href='https://www.abstractapi.com'>Abstract API</a>, the home for modern, developer-friendly  tools like the <a href='https://www.abstractapi.com/ip-geolocation-api'>IP Geolocation API</a>, <a href='https://www.abstractapi.com/vat-validation-rates-api'>VAT Validation & Rates API</a>, <a href='https://www.abstractapi.com/holidays-api'>Public Holiday API</a>, and more.</p>
   </div>
 )
 
