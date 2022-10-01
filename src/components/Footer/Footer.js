ad9bbbd7ac00c039eea01018eaa9bd6aa4be5085
Timmaaaa <tim.utte@outlook.de>
2022-10-01 11:39:35 +0200

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ import getString from '../../lib/strings'
 import styles from './Footer.module.css'
 
 const Footer = () => (
-  <div className={styles.container}>
+  <footer className={styles.container}>
     <div className={styles.social}>
       <FacebookButton />
       <GithubButton />
@@ -21,8 +21,8 @@ const Footer = () => (
     <p>{getString('DEVELOPED_BY')} <a href="https://twitter.com/rogeriopvl">@rogeriopvl</a></p>
     <p>{getString('IMAGES_BY')} Tomomi Imura (<a href="https://twitter.com/girlie_mac">@girlie_mac</a>)</p>
 
-      <p>Check out <a href='https://www.abstractapi.com'>Abstract API</a>, the home for modern, developer-friendly  tools like the <a href='https://www.abstractapi.com/ip-geolocation-api'>IP Geolocation API</a>, <a href='https://www.abstractapi.com/vat-validation-rates-api'>VAT Validation & Rates API</a>, <a href='https://www.abstractapi.com/holidays-api'>Public Holiday API</a>, and more.</p>
-  </div>
+    <p>Check out <a href='https://www.abstractapi.com'>Abstract API</a>, the home for modern, developer-friendly  tools like the <a href='https://www.abstractapi.com/ip-geolocation-api'>IP Geolocation API</a>, <a href='https://www.abstractapi.com/vat-validation-rates-api'>VAT Validation & Rates API</a>, <a href='https://www.abstractapi.com/holidays-api'>Public Holiday API</a>, and more.</p>
+  </footer>
 )
 
 export default Footer
