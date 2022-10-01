4c3559949c1cea367f48111886a7e82a58cb4d4e
Timmaaaa <tim.utte@outlook.de>
2022-10-01 11:24:39 +0200

    accessibility-basic-0

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,11 +6,11 @@ import styles from './Header.module.css'
 
 const Header = () => (
   <header className={styles.container}>
-    <a href="/" className={styles.link}>
-      <div className={styles.logo}>
+    <a href="/" className={styles.link} aria-labelledby="headerTitle" >
+      <div className={styles.logo} tabIndex="-1" aria-hidden >
         <Logo width="80" height="55" color="#d0383e" />
       </div>
-    <h1 className={styles.title}>
+    <h1 id="headerTitle" className={styles.title} aria-level="1">
       {getString('APP_TITLE')}
     </h1>
   </a>
