5483e47e38f8a2f5f4ab6722ce62385886c11a54
Rogério Vicente <rogerio@hey.com>
2023-06-03 16:31:13 +0100

    feat: add status page (#157)

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,12 +1,21 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 import FacebookButton from '@/components/FacebookButton';
 import GithubButton from '@/components/GithubButton';
 import TwitterButton from '@/components/TwitterButton';
+
 import styles from './Footer.module.css';
 
 import lang from '@/locales/en/common.json';
 
 const Footer = () => {
+  const pathname = usePathname();
+
+  const isMainPage = pathname === '/';
+
   return (
     <div className={styles.container}>
       <div className={styles.social}>
@@ -30,22 +39,24 @@ const Footer = () => {
         <a href="https://twitter.com/girlie_mac">@girlie_mac</a>)
       </p>
 
-      <p>
-        Check out <a href="https://www.abstractapi.com">Abstract API</a>, the
-        home for modern, developer-friendly tools like the{' '}
-        <a href="https://www.abstractapi.com/ip-geolocation-api">
-          IP Geolocation API
-        </a>
-        ,{' '}
-        <a href="https://www.abstractapi.com/vat-validation-rates-api">
-          VAT Validation & Rates API
-        </a>
-        ,{' '}
-        <a href="https://www.abstractapi.com/holidays-api">
-          Public Holiday API
-        </a>
-        , and more.
-      </p>
+      {isMainPage && (
+        <p>
+          Check out <a href="https://www.abstractapi.com">Abstract API</a>, the
+          home for modern, developer-friendly tools like the{' '}
+          <a href="https://www.abstractapi.com/ip-geolocation-api">
+            IP Geolocation API
+          </a>
+          ,{' '}
+          <a href="https://www.abstractapi.com/vat-validation-rates-api">
+            VAT Validation & Rates API
+          </a>
+          ,{' '}
+          <a href="https://www.abstractapi.com/holidays-api">
+            Public Holiday API
+          </a>
+          , and more.
+        </p>
+      )}
     </div>
   );
 };
