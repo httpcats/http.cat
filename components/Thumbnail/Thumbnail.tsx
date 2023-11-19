9f897d647762b5b1e98d1fae5d032d42b5aff32f
Alex Mayol <alexmayolc@gmail.com>
2023-11-19 17:04:58 +0100

    feat: lazy lado images

--- components/Thumbnail/Thumbnail.tsx
+++ components/Thumbnail/Thumbnail.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
-
-import styles from './Thumbnail.module.css';
+import Image from 'next/image';
 
 type ThumbnailProps = {
   code: number;
@@ -8,15 +7,20 @@ type ThumbnailProps = {
 };
 
 const Thumbnail = ({ code, description }: ThumbnailProps) => (
-  <div className={styles.container}>
-    <Link href={`/status/${code}`}>
-      <div
-        className={styles.image}
-        style={{ backgroundImage: `url(/images/${code}.jpg)` }}
-      />
-      <div className={styles.content}>
-        <div className={styles.title}>{code}</div>
-        <p>{description}</p>
+  <div className="flex flex-col flex-grow text-white overflow-hidden rounded shadow bg-[--interactive]">
+    <Link href={`/status/${code}`} className='text-white no-underline'>
+      <div className='pt-[56.25%] relative overflow-hidden'>
+        <Image
+          src={`/images/${code}.jpg`}
+          alt=""
+          loading='lazy'
+          fill
+          className="contrast-75 hover:contrast-100 transition duration-500 object-cover scale-[160%] md:scale-[200%] lg:scale-[160%]"
+        />
+      </div>
+      <div className="flex flex-col px-4 pt-4">
+        <div className="text-3xl font-semibold uppercase">{code}</div>
+        <p className='text-base font-semibold'>{description}</p>
       </div>
     </Link>
   </div>
