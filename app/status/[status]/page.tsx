ae3e7f22a6a75957d972a15c26f10ca4b4fdf9ba
rogeriopvl <rogerio@hey.com>
2024-07-09 07:32:58 +0100

    fix(status-page): add missing header and footer

--- app/status/[status]/page.tsx
+++ app/status/[status]/page.tsx
@@ -2,40 +2,49 @@ import { Metadata } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { getStatusInfo } from '@/lib/status-info';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 import StatusDescription from '@/components/StatusDescription';
 
 import statuses from '@/lib/statuses';
+import { getStatusInfo } from '@/lib/status-info';
+import { getTranslations } from '@/lib/translation';
 
 export default async function Info({ params }: { params: { status: string } }) {
   const statusObj = statuses[params.status as unknown as keyof typeof statuses];
   const statusInfoHTML = await getStatusInfo(params.status);
 
+  const t = await getTranslations('en');
+
   return (
-    <main>
-      <nav>
-        <Link href="/" className="text-white">{`< Back to home`}</Link>
-      </nav>
+    <>
+      <Header t={t} />
+      <main>
+        <nav>
+          <Link href="/" className="text-white">{`< Back to home`}</Link>
+        </nav>
 
-      <h1 className="text-center my-12">
-        {statusObj.code} {statusObj.message}
-      </h1>
+        <h1 className="text-center my-12">
+          {statusObj.code} {statusObj.message}
+        </h1>
 
-      <div className="text-center">
-        <Image
-          src={`/images/${statusObj.code.toString()}.jpg`}
-          alt={statusObj.message}
-          width={750}
-          height={600}
-          className="w-full h-full max-w-3xl"
-        />
-      </div>
-      <section className="flex justify-center tracking-wider">
-        <StatusDescription>
-          <div dangerouslySetInnerHTML={{ __html: statusInfoHTML }} />
-        </StatusDescription>
-      </section>
-    </main>
+        <div className="text-center">
+          <Image
+            src={`/images/${statusObj.code.toString()}.jpg`}
+            alt={statusObj.message}
+            width={750}
+            height={600}
+            className="w-full h-full max-w-3xl"
+          />
+        </div>
+        <section className="flex justify-center tracking-wider">
+          <StatusDescription>
+            <div dangerouslySetInnerHTML={{ __html: statusInfoHTML }} />
+          </StatusDescription>
+        </section>
+      </main>
+      <Footer t={t} />
+    </>
   );
 }
 
