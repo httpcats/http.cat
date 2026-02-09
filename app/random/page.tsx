48ef7b50f920fadca4c42e87664b30fa0c03f841
Iván Pérez <48720361+taichikuji@users.noreply.github.com>
2026-02-09 10:12:39 +0000

    Implement static export random page.tsx

--- app/random/page.tsx
+++ app/random/page.tsx
@@ -1,10 +1,19 @@
-import { redirect } from 'next/navigation';
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 import statuses from '@/lib/statuses';
 
 export default function Random() {
-  const statusCodes = Object.keys(statuses);
-  const randomStatus = statusCodes[Math.floor(Math.random() * statusCodes.length)];
-  
-  redirect(`/status/${randomStatus}`);
+  const router = useRouter();
+
+  useEffect(() => {
+    const statusCodes = Object.keys(statuses);
+    const randomStatus = statusCodes[Math.floor(Math.random() * statusCodes.length)];
+
+    router.replace(`/status/${randomStatus}`);
+  }, [router]);
+
+  return null;
 }
